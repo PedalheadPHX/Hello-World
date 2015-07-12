f4612d991869813a6b6489bb3c2b2c6aeb26d2cc
Jonathan Keats <jkeats@tgen.org>
2015-07-11 23:58:30 -0700

    Updating script, adding comments

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# Added a comment, how does this get to git?
+
 echo "Hello World"
 echo "GIT should be easier than this I think"
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hello-World
+
+echo "Hello-World"
+echo "Is it just me or is GIT confusing?"
